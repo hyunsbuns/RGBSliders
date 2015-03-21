b4fd65d752dde39bd18e628bf9d750d885fb6471
Steve <Brabble@Brabbles-MacBook-Pro-2.local>
2015-03-21 01:36:08 -0400

    added NSUserDefaults

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -18,11 +18,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var colorSquare: UIView!
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor") {
+            
+            // this is giving instructions to the new view controller
+            
+            let newViewController = segue.destinationViewController as UIViewController
+            newViewController.view.backgroundColor = colorSquare.backgroundColor
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
 
     }
 
@@ -38,6 +55,12 @@ class ViewController: UIViewController {
         let blue = CGFloat(blueSlider.value)
         
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(redSlider.value, forKey: "red")
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
         
     }
     
